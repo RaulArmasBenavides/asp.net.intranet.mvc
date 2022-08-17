05c8d5bc909e4aed05ab52140424e967f36a5bf8
Raul Armas <you@example.com>
2022-08-17 11:26:42 -0500

    updates

--- intranet/LadoServidor/intranet.dataaccess/UsuarioDAO.cs
+++ intranet/LadoServidor/intranet.dataaccess/UsuarioDAO.cs
@@ -4,93 +4,24 @@ using intranet.entity;
 using System.Data.SqlClient;
 using System.Data;
 using System.Collections.Generic;
+using intranet.dataaccess.Util;
 
 namespace intranet.dataaccess
 {
-    public class UsuarioDAO : Service<Usuario>
+    public class UsuarioDAO : IUsuarioDataAccess
     {
         SqlConnection cn;
- 
+        public string CadenaConexion { get; set; }
 
-        //bdcongresoEntities e = new bdcongresoEntities();
-
-        ////crear usuario
-        //public void create(usp_usuarios_listar_all_Result t)
-        //{
-        //    //throw new NotImplementedException();
-        //    e.usp_registrar_usuario(t.usuario, t.clave);
-
-        //}
-        ////eliminar usuario
-        //public void delete(usp_usuarios_listar_all_Result t)
-        //{
-        //    //throw new NotImplementedException();
-        //    e.usp_eliminar_usuario(t.usuario);
-        //}
-        ////encontrar usuarios
-        //public usp_usuarios_listar_all_Result find(usp_usuarios_listar_all_Result t)
-        //{
-        //    throw new NotImplementedException();
-        //}
-        ////listar usuarios
-        //public List<usp_usuarios_listar_all_Result> readAll()
-        //{
-        //    return e.usp_usuarios_listar_all().ToList();
-        //}
-        ////actualizar usuarios
-        //public void update(usp_usuarios_listar_all_Result t)
-        //{
-        //    e.usp_actualizar_usuario(t.usuario, t.clave);
-        //}
-
-
-        ////string usuario, string clave
-
-        //public ObjectResult<usp_ValidaUsuario_Result> validar(usp_usuarios_listar_all_Result t)
-        //{
-
-        //    return  e.usp_ValidaUsuario(t.usuario, t.clave);
-        //    //try
-        //    //{
-        //    //    if (Convert.ToInt32(e.usp_ValidaUsuario(t.usuario, t.clave)) == 1)
-        //    //        return 1;
-        //    //    else
-        //    //        return 0;
-        //    //}
-        //    //catch (SqlException ex)
-        //    //{
-        //    //    throw ex;
-        //    //}
-
-        //}
-
-        /* public usp_usuarios_listar_all_Result validar(usp_usuarios_listar_all_Result t)
-            {   
-                MessageBox.Show(" 1 " + t.usuario);
-                MessageBox.Show(" 2 " + t.clave);
-
-
-                return e.ValidaUsuario(t.usuario, t.clave);
-                //return e.ValidaUsuario(t.usuario, t.clave);
-                //MessageBox.Show(" el x es " + x);
-
-                /*if(x==1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-               
-
-    }*/
-
+        public UsuarioDAO()
+        {
+            CadenaConexion = CustomXMLReader.leerConexion();//Constantes.CADENA_CONEXION;//ConfigurationManager.ConnectionStrings["neptuno"].ConnectionString;
+        }
         public bool  valida_usuario_exists(Usuario u)
         {
             bool resultado = false;
             List<Usuario> lista = new List<Usuario>();
-            using (cn = new SqlConnection(Constantes.CADENA_CONEXION))
+            using (cn = new SqlConnection(CadenaConexion))
             {
                 var cmd = new SqlCommand("usp_valida_usuario_exists", cn);
                 cmd.CommandType = CommandType.StoredProcedure;
@@ -137,6 +68,36 @@ namespace intranet.dataaccess
         }
 
         public void update(Usuario o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void create(Alumno t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(Alumno t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(Alumno t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Alumno findForId(Alumno t)
+        {
+            throw new NotImplementedException();
+        }
+
+        Alumno IUsuarioDataAccess.findForId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Alumno> IUsuarioDataAccess.readAll()
         {
             throw new NotImplementedException();
         }
---
+++ intranet/LadoServidor/intranet.service/IUsuarioDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using intranet.entity;
+
+namespace intranet.service
+{
+    public interface IUsuarioDataAccess
+    {
+        //definir las firmas
+        void create(Alumno t);
+        void update(Alumno t);
+        void delete(Alumno t);
+        Alumno findForId(Alumno t);
+        Alumno findForId(int id);
+        List<Alumno> readAll();
+    }
+}
