7aa8eebd3fcff6d7cf607be5f4af9a2f760d1264
Raul Armas <you@example.com>
2023-09-22 06:46:47 -0500

    updates

---
+++ intranet/LadoCliente/intranetMVC/Controllers/ReportePreMatriculaController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace intranetMVC.Controllers
+{
+    public class ReportePreMatriculaController : Controller
+    {
+        // GET: ReportePreMatricula
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // Obtener el nombre del archivo
+                string fileName = Path.GetFileName(file.FileName);
+
+                // Guardar el archivo en una ubicación específica
+                string filePath = Path.Combine(Server.MapPath("~/Uploads"), fileName);
+                file.SaveAs(filePath);
+
+                // Mostrar un mensaje de éxito
+                ViewBag.Message = "File uploaded successfully.";
+            }
+            else
+            {
+                // Mostrar un mensaje de error si no se selecciona ningún archivo
+                ViewBag.Message = "Please select a file.";
+            }
+
+            return View("Index");
+        }
+    }
+}
